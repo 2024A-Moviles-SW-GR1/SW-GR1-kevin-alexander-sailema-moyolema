e7b1df5aafac6dd8375a50de2be34b1617ec9cbe
KevinSailema <kevin.sailema@epn.edu.ec>
2024-06-12 13:46:43 -0500

    Clase 07-06-2024 Hasta pantalla ciclo de vida

---
+++ 01-Clases/app/src/main/java/com/example/a2024aswgrsmka/ACicloVida.kt
@@ -0,0 +1,55 @@
+package com.example.a2024aswgrsmka
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+
+class ACicloVida : AppCompatActivity() {
+    var textoGlobal = ""
+
+    fun mostrarSnackbar(texto:String) {
+        textoGlobal += texto
+        val snack = Snackbar.make(
+            findViewById(R.id.cl_ciclo_vida),
+            textoGlobal,
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snack.show()
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aciclo_vida)
+        mostrarSnackbar("onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mostrarSnackbar("onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        mostrarSnackbar("onResume")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        mostrarSnackbar("onRestart")
+    }
+    override fun onPause() {
+        super.onPause()
+        mostrarSnackbar("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mostrarSnackbar("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mostrarSnackbar("onDestroy")
+    }
+}
--- 01-Clases/app/src/main/java/com/example/a2024aswgrsmka/MainActivity.kt
+++ 01-Clases/app/src/main/java/com/example/a2024aswgrsmka/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.a2024aswgrsmka
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,26 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        val botonACicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        botonACicloVida
+            .setOnClickListener{
+                irActividad(ACicloVida::class.java)
+            }
+
+        /* enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_ciclo_vida)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        } */
+    }
+
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
